1f7ef1fd9e34b629f7191ac8688438e7d23a9dd5
bhering <bernhard.hering@slub-dresden.de>
2023-04-24 00:02:49 +0200

    added DC PWM support for non PWM fans connected to mainboard, also added watchdog for pump connected to mobo-fan-header

--- fancontrol.py
+++ fancontrol.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python3
 
 import json
+import os
+import sys
 from time import sleep
 from liquidctl import find_liquidctl_devices
+
+
+def eprint(*args, **kwargs):
+    '''
+    print to stderr
+    '''
+    print(*args, file=sys.stderr, **kwargs)
 
 class Fanspeed:
     def __init__(self, dev):
@@ -10,13 +19,17 @@ class Fanspeed:
         self.oldspeed = -1
         self.dev = dev
         self.rpm = 0
+        self.dc = self.find_dc_device("it86")
         self.init_status = self.dev.initialize()
         self.water_temp = 0
         self.get_temp()
         self.set_speed_all(100)
+        self.set_dc_speed("pwm1",255)
+        self.set_dc_speed("pwm2",255)
         while self.rpm < 2000:
             self.get_rpm()
             sleep(1)
+        self.set_dc_speed("pwm1",200)
 
     def set_speed_all(self, speed):
         if speed == self.oldspeed:
@@ -29,10 +42,15 @@ class Fanspeed:
                     fan_set = True
                 except:
                     pass
+        self.set_dc_speed("pwm2",self.temp_to_dc_pwm())
         self.get_rpm()
-        print("Water has {}°C, set Fans to {}%".format(self.water_temp,speed))
+        eprint("Water has {}°C, set Fans to {}%".format(self.water_temp,speed))
         self.oldspeed = speed
         self._set_attempts = 0
+
+    def set_dc_speed(self,name,value):
+        with open(self.dc+name,"wt") as outp:
+            outp.write(str(value))
 
     def get_rpm(self):
         rpm = -1
@@ -56,7 +74,17 @@ class Fanspeed:
         while True:
             self.get_temp()
             self.set_speed_all(self.temp_to_pwm())
+            self.watch_pump("fan1_input")
             sleep(60)
+
+    def temp_to_dc_pwm(self):
+        pwm = int((self.water_temp-30)*12.5)
+        if pwm < 100:
+            return 0
+        elif pwm > 255:
+            return 255
+        else:
+            return pwm
 
     def temp_to_pwm(self):
         pwm = int((self.water_temp-30)*5)
@@ -67,6 +95,19 @@ class Fanspeed:
         else:
             return pwm
 
+    def find_dc_device(self, name):
+        basepath = "/sys/class/hwmon/"
+        for f in os.listdir(basepath):
+            with open(basepath+f+"/name","rt") as inp:
+                if name in inp.read():
+                    return basepath+f+"/"
+
+    def watch_pump(self, name):
+        with open(self.dc+name,"rt") as inp:
+            rpm = int(inp.read())
+            if rpm == 0:
+                eprint("PUMP NOT RUNNING! Setting Voltage Control to 12V!")
+                self.set_dc_speed("pwm1",255)
 
 def run():
     devices = find_liquidctl_devices()
